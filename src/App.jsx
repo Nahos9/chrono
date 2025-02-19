7d6cb894f9bf1c1b0ed9b671b46178e0bc625be5
nahos9 <garcianahos9@gmail.com>
2025-02-19 14:18:19 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 
+import { useEffect } from 'react'
 import './App.scss'
 
 import Chrono from "./components/Chrono"
 
 function App() {
 
+  useEffect(()=>{
+    document.title = "Chromo"
+  },[])
 
   return (
     <>
