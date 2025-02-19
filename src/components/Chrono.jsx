28e0cd012a98ef49bf7b774b2178d178680ebdc6
nahos9 <garcianahos9@gmail.com>
2025-02-19 14:15:45 +0100

    first commit

--- src/components/Chrono.jsx
+++ src/components/Chrono.jsx
@@ -1,7 +1,27 @@
+import "./chrono.scss"
 export default function Chrono(){
+
     return(
         <>
-            <h1>Deployment in vercel</h1>
+            <div className="container-chrono anime">
+                <div className="config">
+                    <div className="btns">
+                        <button className="btn-moins">-</button>
+                        <span>Miniutes : 5</span>
+                        <button className="btn-plus">+</button>
+                    </div>
+                    <div className="">
+                    <button className="btn-moins">-</button>
+                        <span>Pause : 5</span>
+                        <button className="btn-plus">+</button>
+                    </div>
+                </div>
+               <h1>30:00</h1>
+               <div className="controls">
+                <button className="play">Play</button>
+                <button className="reset">Reset</button>
+               </div>
+            </div>
         </>
     )
 }
